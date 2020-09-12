411f56d4af6923757e82255185d8204b214426d5
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-12 21:16:29 +0300

    Updates

---
+++ precache-manifest.47690d09b5a475fe65d54eda96468530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa901b6869029843af9fa451b304468",
+    "url": "/speed-typing-game/index.html"
+  },
+  {
+    "revision": "26c4bdf697dbdca076d1",
+    "url": "/speed-typing-game/static/css/main.aa5f1dd2.chunk.css"
+  },
+  {
+    "revision": "18c228c764d753be372e",
+    "url": "/speed-typing-game/static/js/2.e54ca885.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speed-typing-game/static/js/2.e54ca885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c4bdf697dbdca076d1",
+    "url": "/speed-typing-game/static/js/main.7944271d.chunk.js"
+  },
+  {
+    "revision": "294e6ac9cd4f7e7f2592",
+    "url": "/speed-typing-game/static/js/runtime-main.b1de8fe3.js"
+  }
+]);
